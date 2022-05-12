b46178e214434bc75c7056620071c54ebff710c5
Cveinnt <30604389+Cveinnt@users.noreply.github.com>
2022-05-12 22:25:09 +0800

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -23,7 +23,7 @@ export const getWeather = async (city: string) => {
 };
 
 export const getQuote = async () => {
-  const { data } = await axios.get('http://api.quotable.io/random');
+  const { data } = await axios.get('https://api.quotable.io/random');
   return {
     quote: `“${data.content}” — ${data.author}`,
   };
