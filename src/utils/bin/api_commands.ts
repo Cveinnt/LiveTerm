5b44f60e040871800e1654bc9c3fc1c25ac4865c
Zhe <satoi8080@users.noreply.github.com>
2022-06-07 15:08:28 +0900

    FIX abnormal spaces at start of ${readme}
    Removed extra newline code
    which causes abnormal line indentation at the start of ${readme} with extra 2 spaces

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -22,8 +22,7 @@ export const quote = async (args: string[]): Promise<string> => {
 
 export const readme = async (args: string[]): Promise<string> => {
   const readme = await getReadme();
-  return `Opening GitHub README...\n
-  ${readme}`;
+  return `Opening GitHub README...\n${readme}`;
 };
 
 export const weather = async (args: string[]): Promise<string> => {
