1ead55def8495a39ceade1dc9c82ea5d7b375c94
Yipeng Liu <liu@yipe.ng>
2023-05-23 19:58:41 +0800

    Ready for deploy.

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -12,13 +12,13 @@ const sumfetch = async (args: string[]): Promise<string> => {
 @@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
 @@                                   @@     -----------
 @@             .@@@@@@@@@@.          @@      CONTACT 
- @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-  @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+ @@           @@          @@        @@       ${config.social.phone}
+  @@           @@        @@        @@        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+   @@             @@@@@@          @@         <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
      @@@                        @@@         -----------
         @@@                  @@@ @@          DONATE 
-         @|  @@@@@@@@@@@@@@@@   @@           <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
-         @|                      @@          <u><a href="${config.donate_urls.patreon}" target="_blank">${config.donate_urls.patreon}</a></u>
+         @|  @@@@@@@@@@@@@@@@   @@           <u><a href="${config.fake}" target="_blank">${config.donate_urls.paypal}</a></u>
+         @|                      @@          <u><a href="${config.fake}" target="_blank">${config.donate_urls.patreon}</a></u>
 
 `;
   } else {
